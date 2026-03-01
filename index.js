368a811c27b026cb8b8d9b25b3788a6f4e4fcaa4
Lucien <lightfireo051@gmail.com>
2026-03-01 13:14:58 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Ash the Almighty is awake 🔥');
+  }
+});
+
+client.login(process.env.TOKEN);
